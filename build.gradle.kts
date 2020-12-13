7b4410d1c62a6fa8a659d13586e2cf9c981c2dcb
Harry Chan <harry.chan@mensa.org.hk>
2020-12-13 09:16:50 +0800

    changed to use settings.gradle.kts to manage the gradle plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 val ktor_version: String by project
-val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm")
 }
 
 group = "poc"
@@ -20,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "ktor-poc"
+
+pluginManagement {
+    val kotlin_version: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlin_version
+    }
+}
